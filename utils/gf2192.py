d234ed14abcc6a384c7a89317d526b89d99460e4
ming-shing chen <mschen@crypto.tw>
2023-06-25 09:37:43 +0800

    update fri_ldt

--- utils/gf2192.py
+++ utils/gf2192.py
@@ -6,13 +6,21 @@ from libgf264fft import clib_wrapper as gf264
 
 GF_EXT = 3
 
+GF_BSIZE = 8*3
+
+def from_bytes( bv ): return int.from_bytes(bv,byteorder='little')
+
+def from_bytes_x2( bb ): return [ int.from_bytes(bb[:GF_BSIZE],byteorder='little'), int.from_bytes(bb[GF_BSIZE:2*GF_BSIZE],byteorder='little') ]
+
+def to_bytes( a ): return a.to_bytes(GF_BSIZE,byteorder='little')
+
+def from_gf264s( a0 , a1 , a2 ): return a0|(a1<<64)|(a2<<128)
+
+def to_gf264s(a) : return a&0xffffffffffffffff,(a>>64)&0xffffffffffffffff,(a>>128)&0xffffffffffffffff
+
 def gf2192_mul( a , b ):
-    a0 = a&0xffffffffffffffff
-    a1 = (a>>64)&0xffffffffffffffff
-    a2 = (a>>128)&0xffffffffffffffff
-    b0 = b&0xffffffffffffffff
-    b1 = (b>>64)&0xffffffffffffffff
-    b2 = (b>>128)&0xffffffffffffffff
+    a0, a1, a2 = to_gf264s(a)
+    b0, b1, b2 = to_gf264s(b)
     c0 = gf264.gf264_mul(a0,b0)
     c1 = gf264.gf264_mul(a0,b1)^gf264.gf264_mul(a1,b0)
     c2 = gf264.gf264_mul(a0,b2)^gf264.gf264_mul(a1,b1)^gf264.gf264_mul(a2,b0)
