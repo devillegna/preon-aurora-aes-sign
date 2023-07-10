046190b444a9ed6b941eb622ef3b90e62b70bc0b
ming-shing chen <mschen@crypto.tw>
2023-07-10 17:34:19 +0800

    use hashlib.sha3_256 instead of Crypto.Hash.SHA3_256

--- commit/merkeltreecommit.py
+++ commit/merkeltreecommit.py
@@ -8,20 +8,21 @@ from utils import randombytes as utrd
 
 LAMBDA = 256
 
-from Crypto.Hash import SHA3_256 as G
+#from Crypto.Hash import SHA3_256 as G
+import hashlib as G
 
 def commit( msgList ):
     num = len(msgList)
     assert( 2 <= num )
     assert( 0==(num&(num-1)) )   # len(msgList) is a power of 2
     r = [ utrd.randombytes( LAMBDA//8 ) for i in range(num) ]
-    mktree = [ [ G.new(b''.join([msgList[i] , r[i]])).digest() for i in range(num) ]  ]
+    mktree = [ [ G.sha3_256( msgList[i]+r[i] ).digest() for i in range(num) ]  ]
     while( num > 2 ):
         last_layer = mktree[-1]
-        mktree.append([ G.new( b''.join([last_layer[i*2],last_layer[i*2+1]]) ).digest() for i in range(num>>1) ])
+        mktree.append([ G.sha3_256( last_layer[i*2]+last_layer[i*2+1] ).digest() for i in range(num>>1) ])
         num = num//2
     last_layer = mktree[-1]
-    rt = G.new( b''.join([last_layer[0],last_layer[1]]) ).digest()
+    rt = G.sha3_256( b''.join([last_layer[0],last_layer[1]]) ).digest()
     return rt, r , mktree
 
 def open( msg , idx , r , mktree ):
@@ -36,10 +37,10 @@ def batchopen( idxes , mesgs , r , mktree ):
     return [ open(mesgs[idx], idx , r , mktree) for idx in idxes ]
 
 def verify( rt , idx , auth_path ):
-    state = G.new( b''.join([auth_path[0],auth_path[1]]) ).digest()
+    state = G.sha3_256( auth_path[0]+auth_path[1] ).digest()
     for i in range(2,len(auth_path)):
-        if (idx&1) : state = G.new( b''.join([auth_path[i],state]) ).digest()
-        else       : state = G.new( b''.join([state,auth_path[i]]) ).digest()
+        if (idx&1) : state = G.sha3_256( auth_path[i]+state ).digest()
+        else       : state = G.sha3_256( state+auth_path[i] ).digest()
         idx = idx//2
     return state == rt
 
